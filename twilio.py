7eae084ec22406db748c002952ae1e42a2aeefc3
p1sc3s <miles.patterson@protonmail.ch>
2015-05-02 00:43:54 -0500

    Added VisitorSid Cookie

--- twilio.py
+++ twilio.py
@@ -4,12 +4,13 @@ import json
 import sqlite3
 from bs4 import BeautifulSoup
 import urllib
+
 '''
 This program is a work in progress that automates carrier lookup information lookup from Twilio's demo website and
 writes it into a database for later use. This program is for demonstration and education purposes only.
 
 '''
- 
+
 #Create SQLite3 DB
 db = sqlite3.connect('phones.db')
 cursor = db.cursor()
@@ -17,7 +18,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS Twilio_Data
                 (phone_number text, country_code text, carrier text, type text)''')
 
 #Path to a list of Phone Numbers to Check. You Need to Change This Manually for Now.
-numberList = open('/usr/share/wordlists/phones.txt', 'r').read().splitlines()
+numberList = open('phones.txt', 'r').read().splitlines()
 
 
 #These Headers Work. Change User-Agent if you feel like it.
@@ -26,12 +27,14 @@ headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:31.0) Gecko/2010010
         'Host': 'www.twilio.com',
         'content-length': '158'}
 
-#Grab A CSRF Token
+#Grab A CSRF Token and visitorSid
 r = requests.get('http://www.twilio.com/lookup')
 soupedupText = BeautifulSoup(r.text)
-csrfToken = soupedupText.find("meta",{"name" : "csrfToken"})['content']
+csrfToken = soupedupText.find("meta", {"name": "csrfToken"})['content']
 csrfToken_Urlized = urllib.quote(csrfToken)
-payload = 'CSRF=' + csrfToken_Urlized
+visitorSid = soupedupText.find('input', {'role': 'visitorSid'})['value']
+
+payload = 'VisitorSid=' + visitorSid + '&CSRF=' + csrfToken_Urlized
 
 for number in numberList:
         r = requests.post("http://www.twilio.com/functional-demos?Type=lookup&PhoneNumber=" + number, data=payload, headers=headers)
@@ -42,7 +45,7 @@ for number in numberList:
         print json.dumps(jsonData, indent=3, sort_keys=True)
         carrierName = jsonData['body']['carrier']['name']
         countryCode = jsonData['body']['country_code']
-	numberType = jsonData['body']['carrier']['type']
+        numberType = jsonData['body']['carrier']['type']
 #        print countryCode
 #        print carrierName
         cursor.execute(
